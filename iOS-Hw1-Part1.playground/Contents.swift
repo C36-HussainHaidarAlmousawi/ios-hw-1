3c1c8137dcb76f968299d543923ee79ea0ad6257
C36-HussainHaidarAlmousawi <70634196+C36-HussainHaidarAlmousawi@users.noreply.github.com>
2020-09-09 21:44:15 +0300

    part 1 done

--- iOS-Hw1-Part1.playground/Contents.swift
+++ iOS-Hw1-Part1.playground/Contents.swift
@@ -5,7 +5,7 @@
  
  */
 
-let name = "Ahmad"
+var name = "Ahmad"
 
 name = "Salem"
 
@@ -15,11 +15,11 @@ var FriendWeight = 80.5
 var height = 1.5
 
 
-var weightSum = myWeight + FriendWeight
+var weightSum = Double(myWeight) * FriendWeight
 
 
 
-var bmi = myWeight * (height * height)
+var bmi = Double(myWeight) * (height * height)
 
 /*
 - - - -
@@ -38,35 +38,37 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "hussain"
+var lastName1 = "almousawi"
+var email1 = "almousawi2006@gmail.com"
+var phoneNumber1 = "66345858"
+var age1 = 14
+var country1 = "kuwait"
+var area1 = "jabria"
+var password1 = "12345678"
+var isKuwaiti1 = true
 
 //Person 2
-
-
-
-
-
-
-
-
-
+var firstName2 = "mohammed"
+var lastName2 = "almousawi"
+var email2 = "12345678@gmail.com"
+var phoneNumber2 = "12345678"
+var age2 = 18
+var country2 = "kuwait"
+var area2 = "jabria"
+var password2 = "12345678"
+var isKuwaiti2 = true
 
 //Person 3
-
-
-
-
-
-
-
+var firstName3 = "ali"
+var lastName3 = "almousawi"
+var email3 = "87654321@gmail.com"
+var phoneNumber3 = "87654321"
+var age3 = 23
+var country3 = "kuwait"
+var area3 = "jabria"
+var password3 = "87654321"
+var isKuwaiti3 = true
 
 
 /*
@@ -82,3 +84,20 @@ var isKuwaiti = false
 
  
  */
+var age_equal = (age1 == age2) || (age1 == age3) || (age2 == age3)
+var area_wqual = (area1 == area2) || (area1 == area3) || (area2 == area3)
+var age1_bigger_18 = age1 > 18
+var age2_bigger_18 = age2 > 18
+var age3_bigger_18 = age3 > 18
+
+var age1_less_18 = age1 < 18
+var age2_less_18 = age2 < 18
+var age3_less_18 = age3 < 18
+
+var age1_equal_17 = age1 == 17
+var age2_equal_17 = age2 == 17
+var age3_equal_17 = age3 == 17
+
+var age1_not_equal_17 = age1 != 17
+var age2_not_equal_17 = age2 != 17
+var age3_not_equal_17 = age3 != 17
